97e8e86549220660d06fbd83a34f97adb3582b95
eliotmonaco <eliotmonaco@gmail.com>
2025-03-26 16:21:07 -0500

    update _output.yml to hide code chunks, add render.R to NB1 template dir

---
+++ inst/rmarkdown/templates/nb1/skeleton/render.R
@@ -0,0 +1,11 @@
+rmd_name <- ""
+
+rmarkdown::render(
+  input = paste0("scripts/", rmd_name, ".rmd"),
+  output_file = paste(
+    rmd_name,
+    format(Sys.time(), "%Y-%m-%d"),
+    sep = "-"
+  ),
+  output_dir = "output"
+)
