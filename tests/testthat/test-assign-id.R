72b15f11b80b61dc3370c108504fa1bcb7b4a20e
eliotmonaco <eliotmonaco@gmail.com>
2025-04-02 14:59:19 -0500

    add test for assign_id()

--- tests/testthat/test-assign-id.R
+++ tests/testthat/test-assign-id.R
@@ -1,4 +1,4 @@
-test_that("works", {
+test_that("fn works", {
   df <- data.frame(
     row_id = paste0("r", 1:24),
     var1 = rep(LETTERS[1:3], 8),
@@ -13,6 +13,27 @@ test_that("works", {
     promote = TRUE
   )
   exp <- cbind("id" = rep(sprintf("id%02d", 1:12), 2), df)
+  expect_equal(act, exp)
+})
+
+test_that("fn works with NA", {
+  df <- data.frame(
+    row_id = paste0("r", 1:24),
+    var1 = rep(LETTERS[1:3], 8),
+    var2 = 1:4
+  )
+  df[c(1, 13), c("var1", "var2")] <- NA
+  df[14, "var1"] <- NA
+  df$var3 <- duplicated(df[, 2:3])
+  act <- assign_id(
+    df,
+    id_name = "id",
+    prefix = "id",
+    vars = c("var1", "var2"),
+    promote = TRUE
+  )
+  exp <- cbind("id" = rep(sprintf("id%02d", 1:12), 2), df)
+  exp[14, "id"] <- "id13"
   expect_equal(act, exp)
 })
 
