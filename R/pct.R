f52b90d74bee5d6d00c112026dabb22ed39395c3
eliotmonaco <eliotmonaco@gmail.com>
2025-09-15 13:39:30 -0500

    added formatting functionality to pct()

--- R/pct.R
+++ R/pct.R
@@ -1,13 +1,16 @@
-#' Calculate percentage
+#' Calculate percentages
 #'
 #' @description
 #' Calculate a percentage using the formula \eqn{n * 100 / total}. Rounding is
 #' done by [round_ties_away()].
 #'
-#' @param n,total Numeric vectors of equal length or with one of length 1.
+#' @param n,total Numeric vectors of equal length or with at least one vector of
+#' length 1.
 #' @param digits An integer indicating the number of decimal (when positive) or
 #' whole number (when zero or negative) places to round the output. (See the
 #' same argument in [round_ties_away()].)
+#' @param format Logical indicating if the output should be formatted with a "%"
+#' sign and if percentages that round to 0 should be replaced with "< 1%".
 #'
 #' @return A numeric vector the same length as the longer of `n` or `total.`
 #' @export
@@ -17,7 +20,7 @@
 #'
 #' pct(sqrt(x), 100, digits = 2)
 #'
-pct <- function(n, total, digits = 1) {
+pct <- function(n, total, digits = 0, format = FALSE) {
   if (
     length(n) != length(total) &&
       length(n) != 1 &&
@@ -32,5 +35,25 @@ pct <- function(n, total, digits = 1) {
   n <- as.numeric(n)
   total <- as.numeric(total)
 
-  setmeup::round_ties_away(n / total * 100, digits = digits)
+  pct <- setmeup::round_ties_away(n / total * 100, digits = digits)
+
+  if (format) {
+    df <- data.frame(n, pct)
+
+    df$fmt <- unlist(apply(df, 1, \(x) {
+      if (x[2] != 0 || (x[1] == 0 && x[2] == 0)) {
+        x[2]
+      } else if (x[1] > 0 && x[2] == 0) {
+        # "< 1"
+        paste("<", 1 / 10^digits)
+      } else if (x[1] < 0 && x[2] == 0) {
+        # "> -1"
+        paste(">", -1 / 10^digits)
+      }
+    }))
+
+    paste0(df$fmt, "%")
+  } else {
+    pct
+  }
 }
--- tests/testthat/test-pct.R
+++ tests/testthat/test-pct.R
@@ -11,7 +11,8 @@ test_that("numeric input", {
 test_that("lists of various classes", {
   act <- suppressWarnings(pct(
     n = list(0, 1, "2", 3, 4, "5", 6, "A", TRUE),
-    total = list(FALSE, "B", "6", 5, "4", 3, 2, 1, 0)
+    total = list(FALSE, "B", "6", 5, "4", 3, 2, 1, 0),
+    digits = 1
   ))
   exp <- c(NaN, NA, 33.3, 60.0, 100.0, 166.7, 300.0, NA, Inf)
   expect_equal(act, exp)
@@ -20,7 +21,8 @@ test_that("lists of various classes", {
 test_that("length of `total` is 1", {
   act <- pct(
     n = c(1, 2, 3, 4),
-    total = 12
+    total = 12,
+    digits = 1
   )
   exp <- c(8.3, 16.7, 25.0, 33.3)
   expect_equal(act, exp)
@@ -46,4 +48,21 @@ test_that("unequal length error", {
       "or one must be of length 1"
     )
   )
+})
+
+test_that("formatted output", {
+  sq <- seq(-8, 8, 4)
+  act <- data.frame(
+    digneg1 = pct(n = sq, total = 100, digits = -1, format = TRUE),
+    dig0 = pct(n = sq, total = 1000, digits = 0, format = TRUE),
+    dig1 = pct(n = sq, total = 10000, digits = 1, format = TRUE),
+    dig2 = pct(n = sq, total = 100000, digits = 2, format = TRUE)
+  )
+  exp <- data.frame(
+    digneg1 = c("-10%", "> -10%", "0%", "< 10%", "10%"),
+    dig0 = c("-1%", "> -1%", "0%", "< 1%", "1%"),
+    dig1 = c("-0.1%", "> -0.1%", "0%", "< 0.1%", "0.1%"),
+    dig2 = c("-0.01%", "> -0.01%", "0%", "< 0.01%", "0.01%")
+  )
+  expect_equal(act, exp)
 })
